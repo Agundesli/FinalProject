7ec0118b3fd5381f2fbaecae75be926385458882
Abdullah <agundesli06@gmail.com>
2022-02-19 21:09:27 +0300

    Attribute

---
+++ FinalProject/Ders11_Attribute/Ders11_Attribute/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ders11_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Attribute
+            //AttributeTargets
+            //AttributeUsageAttribute
+            //FlagsAttribute
+            //SerializableAttribute
+            //ObsoleteAttribute
+            Customer customer = new Customer { Id = 1, LastName = "Gundeşli", Age = 28 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+    class Customer
+    {
+        //Class içindeki proplarar ilave özellik verebiliriz:Attribute ile
+        public int Id { get; set; }
+        //[RequiredProperty]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Id+" - "+customer.FirstName+" - "+customer.LastName+" - "+customer.Age);
+        }
+    }
+}
