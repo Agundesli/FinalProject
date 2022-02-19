57c56b2f937cbf27479931ac91778223d3b6e8e6
Abdullah <agundesli06@gmail.com>
2022-02-19 21:10:29 +0300

    Attribute

--- FinalProject/Ders11_Attribute/Ders11_Attribute/Program.cs
+++ FinalProject/Ders11_Attribute/Ders11_Attribute/Program.cs
@@ -14,25 +14,51 @@ namespace Ders11_Attribute
             //ObsoleteAttribute
             Customer customer = new Customer { Id = 1, LastName = "Gundeşli", Age = 28 };
             CustomerDal customerDal = new CustomerDal();
-            customerDal.Add(customer);
+            //customerDal.Add(customer);//[Obsolete] ile getirdiğim kısıtlama
+            customerDal.AddNew(customer);
             Console.ReadLine();
         }
     }
+    [ToTable("Customers")]
     class Customer
     {
-        //Class içindeki proplarar ilave özellik verebiliriz:Attribute ile
+        //Class içindeki proplarar ilave özellik,kural verebiliriz:Attribute ile merkezi bir noktada iş kuralları koyabilirim
+        //Değerleri girmek zorunlu mesela.
+        [RequiredProperty]
         public int Id { get; set; }
-        //[RequiredProperty]
+        [RequiredProperty]
         public string FirstName { get; set; }
+        [RequiredProperty]
         public string LastName { get; set; }
+        [RequiredProperty]
         public int Age { get; set; }
 
     }
     class CustomerDal
     {
+        [Obsolete("Dont't use Add, instead use AddNew Method")] 
         public void Add(Customer customer)
         {
             Console.WriteLine(customer.Id+" - "+customer.FirstName+" - "+customer.LastName+" - "+customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine(customer.Id+" - "+customer.FirstName+" - "+customer.LastName+" - "+customer.Age);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple =true)]
+    class RequiredPropertyAttribute:Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tablename;
+
+        public ToTableAttribute(string tablename)
+        {
+            _tablename = tablename;
         }
     }
 }
---
+++ FinalProject/Ders11_Reflection/Ders11_Reflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders11_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
