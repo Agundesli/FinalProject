453195911db9e9ec5c1723a47cd756b491cd491d
Abdullah <agundesli06@gmail.com>
2022-03-18 16:37:18 +0300

    RentaCar

--- CarProject/Business/Concrete/CarManager.cs
+++ CarProject/Business/Concrete/CarManager.cs
@@ -22,6 +22,7 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+        [SecuredOperation]
         [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
